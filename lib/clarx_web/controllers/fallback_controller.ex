b8d12ea98f528533f45aba26123cdd5daf55c032
Tiago de Oliveira <deoliv.tiago@gmail.com>
2024-01-20 18:12:14 -0300

    feat: add fallback controller to handle errors

---
+++ lib/clarx_web/controllers/fallback_controller.ex
@@ -0,0 +1,16 @@
+defmodule ClarxWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use ClarxWeb, :controller
+
+  @doc false
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(json: ClarxWeb.ChangesetJSON)
+    |> render(:error, changeset: changeset)
+  end
+end
