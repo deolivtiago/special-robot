b9d7859f485ed3a44967e9a66fb3b1a01522de90
Tiago de Oliveira <deoliv.tiago@gmail.com>
2024-01-20 18:17:59 -0300

    feat: add controller to handle users

--- lib/clarx_web/router.ex
+++ lib/clarx_web/router.ex
@@ -7,6 +7,8 @@ defmodule ClarxWeb.Router do
 
   scope "/api", ClarxWeb do
     pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   # Enable Swoosh mailbox preview in development
